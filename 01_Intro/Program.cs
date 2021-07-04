52093e35d3e9846a930513b7e48aafe2805632f8
benvaro <benvaro91@gmail.com>
2021-07-04 10:34:11 +0300

    Add project files.

---
+++ 01_Intro/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace _01_Intro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region repeat
+            // ТЗ (на СУБД):
+            // зберігати дані 
+            // формувати звіти 
+            // резервне копіювання і відновлення
+            // система прав користувачів
+            // доступ до даних (по мережі)
+
+            // інтерфейс адміна
+            // API (набір функцій)
+            // універсальна мова - sql
+            // шифрування - захист від несанкціонованого доступу
+
+            // ORM - EntityFramework (EF) 
+            #endregion
+
+            //1) connection string
+            // Data Source - server name
+            //Initial catalog - database
+            // IS - режим аутентифікації, якщо true - windows authentification
+            //                                 false - server authentification
+            string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;
+                                        Initial catalog=master;
+                                        Integrated Security=true;";
+
+            connectionString = "Data Source=10.7.101.197;User ID=test;Password=1;Integrated Security=false;Initial catalog=master;";
+            // 2) створюємо об'єкт з'єднання
+            //SqlConnection connection = new SqlConnection();
+            //connection.ConnectionString = connectionString;
+
+            var connection = new SqlConnection(connectionString);
+            // 3) відкрити з'єднання
+
+            connection.StateChange += Program_StateChange;
+            #region open and close
+            //try
+            //{
+            //    connection.Open();
+            //    Console.WriteLine($"State: {connection.State}");
+            //    //
+            //    //
+            //}
+            //catch (SqlException ex)
+            //{
+            //    Console.WriteLine(ex.Message);
+            //}
+            //finally
+            //{
+            //    if (connection != null)
+            //    {
+            //        connection.Close();
+            //    }
+            //}
+            #endregion
+            using (connection)
+            {
+                try
+                {
+                    connection.Open();
+                    Console.WriteLine($"State: {connection.State}");
+                }
+                catch (SqlException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+            // code
+        }
+
+        private static void Program_StateChange(object sender, StateChangeEventArgs e)
+        {
+            SqlConnection connection = sender as SqlConnection;
+            Console.WriteLine($"Connection:\n\tDatabase: {connection.Database}" +
+                $"\n\tServer: {connection.DataSource}" +
+                $"\n\tState: {connection.State}");
+        }
+    }
+}
---
+++ 02_ConnectToRemote/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace _02_ConnectToRemote
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = @"Data Source=10.7.101.197;Initial Catalog=master;
+                                        Integrated Security=false; User Id=test; Password=1";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    Console.WriteLine(connection.ConnectionString);
+                    Console.WriteLine($"Database: {connection.Database}");
+                    connection.ChangeDatabase("SportShop");
+                    Console.WriteLine($"after change db: {connection.Database}");
+
+                    //     string commandText = "create database TestAdoNet";
+
+                    SqlCommand command = new SqlCommand();
+                    //command.CommandText = commandText;
+                    command.Connection = connection;
+
+                    //        SqlCommand command = new SqlCommand(commandText, connection);
+                    // ExecuteNonQuery - insert, update, delete, create
+                    // ExecuteScalar - select min, max, avg, count, sum
+                    // ExecuteReader - select
+                    //       int result = command.ExecuteNonQuery();
+                    //       Console.WriteLine("After create : " + result);
+
+                    connection.ChangeDatabase("TestAdoNet");
+
+                    // string createTableCommand = "create table Test(Id int identity primary key," +
+                    //                                 "Name nvarchar(20))";
+                    // command.CommandText = createTableCommand;
+                    // command.ExecuteNonQuery();
+                    //Console.WriteLine("Table created");
+
+                    // command.CommandText = "Insert into Test values(N'Alex'), " +
+                    //                                            "(N'Katia')";
+                    //int rows = command.ExecuteNonQuery();
+                    //command.CommandText = "Insert into Test values(N'Valia'), " +
+                    //                                            "(N'Katia')";
+                    //rows = command.ExecuteNonQuery();
+                    //Console.WriteLine($"{rows} rows where added");
+
+                    /// Read from table
+                    /// 
+                    command.CommandText = "select * from Test";
+                    SqlDataReader reader = command.ExecuteReader();
+                    if (reader.HasRows)
+                    {
+                        while (reader.Read())
+                        {
+                            //object id = reader[0];
+                            //object name = reader[1];
+
+                            object id = reader["Id"];
+                            object name = reader["Name"];
+
+                            Console.WriteLine($"{id}\t\t{name}\n");
+                        }
+                    }
+                    reader.Close();
+
+                }
+                catch (SqlException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+    }
+}
